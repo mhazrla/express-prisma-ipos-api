aaa9c738b332dc552a1cff66e0526696d5cccfc7
mhazrla-sqiva <hilmy.azkarillah@sqiva.com>
2025-11-09 15:53:50 +0700

    refactor index file

--- src/routes/customerRoute.ts
+++ src/routes/customerRoute.ts
@@ -12,17 +12,9 @@ const customerRouter = express.Router()
 
 // List with pagination: /api/v1/customers?page=1&limit=10
 customerRouter.get('/customers', getCustomers)
-
-// Create
 customerRouter.post('/customers', validate(createCustomerSchema), createCustomer)
-
-// Get by id
 customerRouter.get('/customers/:id', getCustomerById)
-
-// Update
 customerRouter.put('/customers/:id', validate(updateCustomerSchema), updateCustomer)
-
-// Delete
 customerRouter.delete('/customers/:id', deleteCustomer)
 
 export default customerRouter
